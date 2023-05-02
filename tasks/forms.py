07d322cb75e5f8e7309c84af473cde396b2b735e
Federico Guerrero <fede_guerrero21@hotmail.com>
2023-05-02 10:34:56 -0300

    css del registro

--- tasks/forms.py
+++ tasks/forms.py
@@ -8,10 +8,17 @@ from django.contrib.auth.forms import UserCreationForm
 
 class RegisterForm(UserCreationForm):
 
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'inputs'}))
+    #username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+
 
     class Meta:
         model=User
         fields = ['username', 'email', 'password1', 'password2']
 
+    def __init__(self, *args, **kwargs):
+        super(RegisterForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
