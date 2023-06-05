974c626618ac5a4bc18800a7473c5d8bb61ee62b
gcarreiras <gcarreiras86@gmail.com>
2023-06-05 18:35:35 -0300

    add accesories modal

--- tasks/views.py
+++ tasks/views.py
@@ -11,7 +11,7 @@ from django.contrib.auth.models import User
 def index(request):
     contact_form = ContactForm()
     login_form = LoginForm()
-
+    is_accesories_page = False
 
     if request.method == "POST":
         contact_form = ContactForm(request.POST)
@@ -27,8 +27,8 @@ def index(request):
    
     else:
         contact_form = ContactForm()
-   
-    return render(request, 'index.html',{'contact_form': contact_form, 'login_form':login_form})
+    print('is_accesories_page', is_accesories_page)
+    return render(request, 'index.html',{'contact_form': contact_form, 'login_form':login_form , 'is_accesories_page': is_accesories_page})
 
 def login_user(request):
     if request.method == "POST":
@@ -49,25 +49,6 @@ def logout_user(request):
 
 
 
-def signup(request):
-    login_form = LoginForm()
-    if request.method == "POST":
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            # Crear un nuevo usuario
-            new_user = User.objects.create_user(username=username, password=password)
-            new_user.email = form.cleaned_data['email']
-            #new_user.first_name = form.cleaned_data['first_name']
-            #new_user.last_name = form.cleaned_data['last_name']
-            new_user.save()
-            
-            messages.success(request, "Registro exitoso!")
-            return redirect('index')
-    else:
-        form = RegisterForm()
-    return render(request, 'signup.html', {'form': form, 'login_form':login_form})
 
 # def signup(request):
 #     if request.method == "POST":
@@ -92,8 +73,11 @@ def signup(request):
 #    return render(request, 'signup.html')
 
 def accesories(request):
+
     accessory = Accessory.objects.all()
-    return render(request, 'accesories.html', {'accessory': accessory})
+    is_accesories_page = True
+    print('is_accesories_page', is_accesories_page) 
+    return render(request, 'accesories.html', {'accessory': accessory , 'is_accesories_page': is_accesories_page})
 
 def cars(request):
     return render(request, 'cars.html')
@@ -103,3 +87,70 @@ def contact(request):
 
 #def login(request):
 #    return render(request, 'login.html')
+
+def signup(request):
+    login_form = LoginForm()
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            # Crear un nuevo usuario
+            new_user = User.objects.create_user(username=username, password=password)
+            new_user.email = form.cleaned_data['email']
+            #new_user.first_name = form.cleaned_data['first_name']
+            #new_user.last_name = form.cleaned_data['last_name']
+            new_user.save()
+            
+            messages.success(request, "Registro exitoso!")
+            return redirect('index')
+    else:
+        form = RegisterForm()
+    return render(request, 'signup.html', {'form': form, 'login_form':login_form})
+
+
+#///////////// add accesory from modal //////////////////////////////////
+
+def add_accessory(request):
+    print ('pre-post')
+    if request.method == 'POST':
+        print ('post')
+        name = request.POST.get('name')
+        color = request.POST.get('color')
+        brand = request.POST.get('brand')
+        description = request.POST.get('description')
+        image = request.FILES.get('image')
+        price = request.POST.get('price')
+        username = 'gaston'
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            # Manejo de error si el usuario no existe
+            # Por ejemplo, puedes redirigir a una página de error o mostrar un mensaje al usuario.
+            return HttpResponse('El usuario no existe')
+
+
+        # # Verificar si el usuario está autenticado, esto es a modo de prueba ... despues solo usuario con permisos
+        # if request.user.is_authenticated:
+        #     user = request.user
+        # else:
+            
+        #     user = User.objects.get(username='default_user')
+
+        accessory = Accessory(
+            name=name,
+            color=color,
+            brand=brand,
+            description=description,
+            image=image,
+            price=price,
+            user=user
+        )
+        print('accessory', accessory)
+        accessory.save()
+    return redirect('accesories')
+
+
+    #return render(request, 'accesories.html')
+
+# //////////////// end add accesory //////////////////////////////////
